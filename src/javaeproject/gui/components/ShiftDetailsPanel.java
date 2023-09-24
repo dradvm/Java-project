295a6baa63d31b8f693a353ffb642c7411ccf78c
dradvm <duynnvn2511@gmail.com>
2023-09-24 13:47:53 +0700

    add GUI

--- src/javaeproject/gui/components/ShiftDetailsPanel.java
+++ src/javaeproject/gui/components/ShiftDetailsPanel.java
@@ -5,16 +5,28 @@
 package javaeproject.gui.components;
 
 import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import javaeproject.dao.DepartmentDAO;
+import javaeproject.dao.EmployeeDAO;
+import javaeproject.dao.RoomDAO;
+import javaeproject.dao.ShiftDAO;
+import javaeproject.model.Shift;
+import javaeproject.model.User;
+import javax.swing.ImageIcon;
 
 /**
  *
  * @author DELL
  */
 public class ShiftDetailsPanel extends javax.swing.JPanel {
-
+    
+    private final DepartmentDAO departmentDAO = new DepartmentDAO();
+    private final ShiftDAO shiftDAO = new ShiftDAO();
+    private final RoomDAO roomDAO = new RoomDAO();
+    
     public ShiftDetailsPanel() {
         initComponents();
         setOpaque(false);
@@ -22,27 +34,127 @@ public class ShiftDetailsPanel extends javax.swing.JPanel {
     protected void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D)g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setColor(Color.BLACK);
+        GradientPaint gradientPaint = new GradientPaint(0, 0, Color.decode("#2980b9"), 0, getHeight(), Color.decode("#2c3e50"));
+        g2.setPaint(gradientPaint);
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
         super.paintComponent(g);
     }
+    
+    public void setShift(Shift shift) {
+        switch (shift.getType()) {
+            case "Morning" -> jLabel1.setIcon(new ImageIcon(getClass().getResource("/javaeproject/gui/assests/sun.png")));
+            case "Afternoon" -> jLabel1.setIcon(new ImageIcon(getClass().getResource("/javaeproject/gui/assests/moon.png")));
+            case "Fulltime" -> jLabel1.setIcon(new ImageIcon(getClass().getResource("/javaeproject/gui/assests/24-hours.png")));
+
+            default -> throw new AssertionError();
+        }
+       jLabelDate.setText(shift.getDate().toString());
+       jLabelDepartmentName.setText(departmentDAO.getNameByID(shift.getDepartmentID()));
+       jLabelEmployeeName.setText(shiftDAO.getEmployeeNameAssignToShift(shift));
+       jLabelRoomType.setText(roomDAO.getByID(shift.getRoomID()).getRoomType());
+        repaint();
+    } 
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabelDepartmentName = new javax.swing.JLabel();
+        jLabelDate = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabelEmployeeName = new javax.swing.JLabel();
+        jLabelRoomType = new javax.swing.JLabel();
+
+        jLabelDepartmentName.setBackground(new java.awt.Color(255, 255, 255));
+        jLabelDepartmentName.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabelDepartmentName.setForeground(new java.awt.Color(255, 255, 255));
+
+        jLabelDate.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabelDate.setForeground(new java.awt.Color(255, 255, 255));
+
+        jLabel4.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Employee:");
+
+        jLabel5.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+
+        jLabel6.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Room type:");
+
+        jLabelEmployeeName.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
+        jLabelEmployeeName.setForeground(new java.awt.Color(255, 255, 255));
+
+        jLabelRoomType.setFont(new java.awt.Font("SansSerif", 0, 12)); // NOI18N
+        jLabelRoomType.setForeground(new java.awt.Color(255, 255, 255));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabelDepartmentName, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabelDate, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel6)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabelRoomType, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabelEmployeeName, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(31, 31, 31))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(118, 118, 118)
+                        .addComponent(jLabel5))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(23, 23, 23)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabelDepartmentName, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabelDate, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabelEmployeeName, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabelRoomType, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(40, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabelDate;
+    private javax.swing.JLabel jLabelDepartmentName;
+    private javax.swing.JLabel jLabelEmployeeName;
+    private javax.swing.JLabel jLabelRoomType;
     // End of variables declaration//GEN-END:variables
 }
